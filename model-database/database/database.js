7adebdaac2612e1767efe11ff467f21ded59f51f
Prosper <nkonyeprospero@gmail.com>
2023-11-08 15:31:41 +0100

    changed a few things again

--- model-database/database/database.js
+++ model-database/database/database.js
@@ -1,19 +1,24 @@
-const Sequelize = require("sequelize");
-
-const sequelize = new Sequelize("lyter_shop", "bobapp", "testermain", {
-  dialect: "mysql",
-  host: "localhost",
-});
-
-module.exports = sequelize;
-
-//production
 // const Sequelize = require("sequelize");
 
-// //database //username //password
-// const sequelize = new Sequelize("specifi4_bank", "specifi4_banke", "bankspassword", {
+// const sequelize = new Sequelize("lyter_shop", "bobapp", "testermain", {
 //   dialect: "mysql",
 //   host: "localhost",
 // });
 
 // module.exports = sequelize;
+
+//production
+const Sequelize = require("sequelize");
+
+//database specifi4_lyter  //username specifi4_lyter_db_user //password lyterinc3
+const sequelize = new Sequelize(
+  "specifi4_lyter",
+  "specifi4_lyter_db_user",
+  "lyterinc3",
+  {
+    dialect: "mysql",
+    host: "localhost",
+  }
+);
+
+module.exports = sequelize;
